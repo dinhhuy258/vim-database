7576f80278c1544d46b6ca5fe89cb054a4b64456
Huy Duong <huy.duongdinh@gmail.com>
2021-07-25 15:21:33 +0700

    Config sqls lsp

--- rplugin/python3/database/transitions/lsp_ops.py
+++ rplugin/python3/database/transitions/lsp_ops.py
@@ -1,9 +1,7 @@
-import json
-import os
 from functools import partial
-from hashlib import md5
 from os import path
-from typing import Any, Dict
+
+import yaml
 
 from ..concurrents.executors import run_in_executor
 from ..configs.config import UserConfig
@@ -12,7 +10,11 @@ from ..storages.connection import Connection, ConnectionType
 from ..utils.files import is_file_exists, create_folder_if_not_present
 from ..utils.log import log
 
-_LS_CONFIG_FILE_NAME = ".sqllsrc.json"
+
+class YamlDumper(yaml.Dumper):
+
+    def increase_indent(self, flow=False, *args, **kwargs):
+        return super().increase_indent(flow=flow, indentless=False)
 
 
 async def lsp_config(_: UserConfig, state: State) -> None:
@@ -20,91 +22,55 @@ async def lsp_config(_: UserConfig, state: State) -> None:
         log.info("[vim-database] No connection found")
         return
 
-    await run_in_executor(partial(switch_database_connection, state.selected_connection, state.selected_database))
+    await run_in_executor(partial(_switch_database_connection, state.selected_connection, state.selected_database))
 
 
-def switch_database_connection(connection: Connection, database: str) -> None:
-    current_project_path = os.getcwd()
-    config_connection_name = md5(current_project_path.encode('utf-8')).hexdigest()
-    config_folder = path.join(path.expanduser("~"), ".config/sql-language-server")
-    config_path = path.join(config_folder, _LS_CONFIG_FILE_NAME)
+def _switch_database_connection(connection: Connection, database: str) -> None:
+    config_connection_name = "vim-database"
+    config_folder = path.join(path.expanduser("~"), ".config/sqls")
+    config_path = path.join(config_folder, "config.yml")
 
+    config_connections = []
     if not is_file_exists(config_path):
         create_folder_if_not_present(config_folder)
-        config_file = open(config_path, "w")
-        config_file.write(json.dumps({"connections": []}, indent=2))
-        config_file.close()
-
-    with open(config_path) as config_file:
-        json_data = json.load(config_file)
-        config_connections = json_data["connections"]
-
-    current_config_connection = next((config_connection for config_connection in config_connections
-                                      if config_connection["name"] == config_connection_name), None)
-
-    if current_config_connection is not None and _is_connection_configured(connection, database,
-                                                                           current_config_connection):
-        log.info("[vim-database] Switch database connection successfully")
-        return
+    else:
+        with open(config_path) as config_file:
+            data = yaml.load(config_file)
+            config_connections = data["connections"]
 
     if connection.connection_type == ConnectionType.SQLITE:
         new_config_connection = {
-            "name": config_connection_name,
-            "adapter": "sqlite3",
-            "filename": connection.database,
-            "projectPaths": [current_project_path]
+            "alias": config_connection_name,
+            "driver": "sqlite3",
+            "dataSourceName": connection.database,
         }
-    elif connection.connection_type == ConnectionType.MYSQL:
+    elif connection.connection_type == ConnectionType.MYSQL or connection.connection_type == ConnectionType.POSTGRESQL:
         new_config_connection = {
-            "name": config_connection_name,
-            "adapter": "mysql",
+            "alias": config_connection_name,
+            "driver": "mysql" if connection.connection_type == ConnectionType.MYSQL else "postgres",
             "host": connection.host,
             "port": connection.port,
             "user": connection.username,
-            "password": connection.password,
-            "database": database,
-            "projectPaths": [current_project_path]
-        }
-    elif connection.connection_type == ConnectionType.POSTGRESQL:
-        new_config_connection = {
-            "name": config_connection_name,
-            "adapter": "postgres",
-            "host": connection.host,
-            "port": connection.port,
-            "user": connection.username,
-            "password": connection.password,
-            "database": database,
-            "projectPaths": [current_project_path]
+            "passwd": connection.password,
+            "dbName": database
         }
     else:
         log.info("[vim-database] Connection type is not supported")
         return
 
     for index, config_connection in enumerate(config_connections):
-        if config_connection_name == config_connection["name"]:
+        if config_connection_name == config_connection["alias"]:
             del config_connections[index]
             break
     config_connections.append(new_config_connection)
 
     config_file = open(config_path, "w")
-    config_file.write(json.dumps({"connections": config_connections}, indent=2))
+    config_file.write(
+        yaml.dump({
+            "lowercaseKeywords": False,
+            "connections": config_connections
+        }, Dumper=YamlDumper, sort_keys=False))
+
     config_file.close()
-    log.info("[vim-database] Switch database connection successfully. Please restart sql-language-server")
 
-
-def _is_connection_configured(connection: Connection, selected_database: str, config_connection: Dict[str,
-                                                                                                      Any]) -> bool:
-    if not config_connection["adapter"].lower().startswith(connection.connection_type.to_string().lower()):
-        return False
-
-    if config_connection["adapter"] == "sqlite3":
-        return config_connection["filename"] == selected_database
-
-    if not config_connection["database"] != selected_database:
-        return False
-    if not config_connection["host"] != connection.host:
-        return False
-    if not config_connection["port"] != connection.port:
-        return False
-
-    return True
+    log.info("[vim-database] Switch database connection successfully.")
